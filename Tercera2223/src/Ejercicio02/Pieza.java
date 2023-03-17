df6126444c176bdaf283130845f2c03e9153f11f
Alberto Saz <albertosaz@gmail.com>
2023-03-17 23:19:02 +0100

    piezas moviles falta afinar acierto

---
+++ Tercera2223/src/Ejercicio02/Pieza.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio02;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author alberto
+ */
+public class Pieza extends Rectangle{
+    private Image image;
+    public static final int SIZE = 60;
+    private boolean ok = false;
+    
+    public Pieza(Image imagen){
+        super((int)(Math.random()*400)+500, (int)(Math.random()*450)+50, SIZE, SIZE);
+        this.image = imagen;
+       
+    }
+    
+    public void paint(Graphics g, Applet applet){
+       g.drawImage(image, x, y, applet);
+    }
+    public void update(int x, int y){
+        if(!ok){  
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+   
+}
