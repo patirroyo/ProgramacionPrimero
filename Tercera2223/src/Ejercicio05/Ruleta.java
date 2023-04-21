e7edc1780e34205d4b28071ce8a31761c9a0c3fd
Alberto Saz <albertosaz@gmail.com>
2023-04-21 11:43:16 +0200

    Cartas BlackJack

--- Tercera2223/src/Ejercicio05/Ruleta.java
+++ Tercera2223/src/Ejercicio05/Ruleta.java
@@ -18,10 +18,6 @@ import java.awt.Panel;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author alberto
- */
 public class Ruleta extends Applet implements Runnable {
     private Graphics noseve;
     private Image imagen; 
@@ -69,9 +65,36 @@ public class Ruleta extends Applet implements Runnable {
         boton = new Button("Jugar !");
        // panel.add(boton);
        // this.setLayout(new BorderLayout());
-        this.add("North", boton);
-        
+        this.add("North", boton); 
     }
+    
+    private void instanciarCasillasColores() {
+        for(int i = 0; i < ROWS; i++)
+            for(int j = 0; j < COLUMNS; j++){
+                final int valorCasilla = (i*COLUMNS + j)+1;
+                if(numeroEsNegro(valorCasilla))
+                    casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO, (i*COLUMNS + j) +1, Color.BLACK);
+                else{
+                    casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO, (i*COLUMNS + j) +1, Color.RED);
+                    lRojos.add(new Integer(valorCasilla));
+                }
+            }
+    }
+    
+    private static boolean numeroEsNegro(final int valorCasilla) {
+        return (valorCasilla % 2 == 0 && (valorCasilla <= 10 || ((valorCasilla > 19) && valorCasilla < 29)))||
+                (valorCasilla % 2 == 1 && ((valorCasilla > 10 && valorCasilla < 19)|| valorCasilla >= 29 ));
+    }
+    
+    private void instanciarFichas() {
+        for(int i = 0; i < fichaImagenes.length; i++)
+            fichaImagenes[i] = getImage(getCodeBase(), "Ejercicio05/Fichas/" + "ficha" + (i+1) + ".png");
+        
+        fichas = new Ficha[NFICHAS][NFICHAS];
+        for(int i = 0; i < NFICHAS; i++)
+            for(int j = 0; j < NFICHAS; j++)
+                fichas[i][j] = new Ficha(i*(Ficha.LADO-10)+j+i*15,VALORFICHA[i], fichaImagenes[i]);
+    }   
     
     @Override
     public void start(){
@@ -95,142 +118,18 @@ public class Ruleta extends Applet implements Runnable {
         
         g.drawImage(imagen, 0, 0, SIZEX, SIZEY, this);
     }
-     public void update(Graphics g){ //override, lo sobreescribimos eliminando la linea de borrar
-        paint(g);  
-    }
     
-    public void run(){
-        while(true){  
-        }
-    }
-    
-    public boolean action(Event ev, Object obj){
-        if(ev.target instanceof Button){
-            numeroAleatorio = (int)(Math.random() * 37);
-            guardarUltimas10jugadas();
-            comprobarApuesta();  
-        repaint();
-        return true;
-        }
-           
-        return false;
-    }
-
-    private void comprobarApuesta() {
-        acumuladorUltimaJugada = 0;
-        for(int i = 0; i < NFICHAS; i++)
-            for(int j = 0; j < NFICHAS; j++){
-                comprobarApuestaANumero(i, j);
-                restarValorFichaApostada(i, j);
-            }
-        saldo += acumuladorUltimaJugada;
-    }
-
-    private void comprobarApuestaANumero(int i, int j) {
-        for(Integer elemento : fichas[i][j].getApuestas())
-            if(numeroAleatorio == elemento){
-                acumuladorUltimaJugada += fichas[i][j].getValor() * NUMCASILLAS/fichas[i][j].getApuestas().size();
-                System.out.println("valor ficha: " + fichas[i][j].getValor());
-                System.out.println("tamaño lista apuesta : " + fichas[i][j].getApuestas().size());
-                System.out.println("acumulado: " + fichas[i][j].getValor() * NUMCASILLAS/fichas[i][j].getApuestas().size());
-            }
-    }
-    private void restarValorFichaApostada(int i, int j) {
-        if(!fichas[i][j].getApuestas().isEmpty()){
-            acumuladorUltimaJugada -= fichas[i][j].getValor();
-            System.out.println("Ficha jugada: -" + fichas[i][j].getValor());
-        }
-    }
-    
-    private void guardarUltimas10jugadas() {
-        for(int i = ultimas10jugadas.length - 1 ; i > 0 ; i--)
-            ultimas10jugadas[i] = ultimas10jugadas[i -1];
-        ultimas10jugadas[0] = numeroAleatorio;
-    }
-    
-    @Override
-    public boolean mouseDown(Event ev, int x, int y){
-       for(int i = 0; i < NFICHAS; i++)
-            for(int j = 0; j < NFICHAS; j++)
-                if(fichas[i][j].contains(x, y))
-                    fichaActual = fichas[i][j];
-        fichaActual.clearApuestas();
-        return true;
-    } 
-    @Override
-    public boolean mouseDrag(Event ev, int x, int y){
-        if(fichaActual == null)
-            return false;
-        
-        fichaActual.move(x, y);
-        repaint();
-        return true;
-    }
-    @Override
-    public boolean mouseUp(Event ev, int x, int y){
-        if(fichaActual == null)
-            return false;
-                 
-        fichaActual.setApuestas(casillas);         
-        fichaActual = null;
-            
-        return true;
-    }
-    private void instanciarFichas() {
-        for(int i = 0; i < fichaImagenes.length; i++)
-            fichaImagenes[i] = getImage(getCodeBase(), "Ejercicio05/Fichas/" + "ficha" + (i+1) + ".png");
-        
-        fichas = new Ficha[NFICHAS][NFICHAS];
-        for(int i = 0; i < NFICHAS; i++)
-            for(int j = 0; j < NFICHAS; j++)
-                fichas[i][j] = new Ficha(i*(Ficha.LADO-10)+j+i*15,VALORFICHA[i], fichaImagenes[i]);
-    }
-
-    private void instanciarCasillasColores() {
-        for(int i = 0; i < ROWS; i++)
-            for(int j = 0; j < COLUMNS; j++){
-                final int valorCasilla = (i*COLUMNS + j)+1;
-                if(numeroEsNegro(valorCasilla))
-                    casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO, (i*COLUMNS + j) +1, Color.BLACK);
-                else{
-                    casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO, (i*COLUMNS + j) +1, Color.RED);
-                    lRojos.add(new Integer(valorCasilla));
-                }
-            }
-    }
-   
-
-    private static boolean numeroEsNegro(final int valorCasilla) {
-        return (valorCasilla % 2 == 0 && (valorCasilla <= 10 || ((valorCasilla > 19) && valorCasilla < 29)))||
-                (valorCasilla % 2 == 1 && ((valorCasilla > 10 && valorCasilla < 19)|| valorCasilla >= 29 ));
-    }
-
-    private void paintUltimas10jugadas() {
-        noseve.setColor(Color.BLACK);
-        for(int i = 0; i < ultimas10jugadas.length; i++){
-            noseve.drawRect(300 + 50*i, 300, 50, 50);
-            if(ultimas10jugadas[i] == 0)
-                noseve.setColor(Color.WHITE);
-            else if(lRojos.contains(ultimas10jugadas[i]))
-                noseve.setColor(Color.RED);
-            else
-                noseve.setColor(Color.BLACK);            
-            noseve.drawString(Integer.toString(ultimas10jugadas[i]), 310 + 50*i, 335);
-            noseve.setColor(Color.BLACK);
-        }
-    }
-
-    private void paintFichas() {
-        for(int i = 0; i < NFICHAS; i++)
-            for(int j = 0; j < NFICHAS; j++)
-                fichas[i][j].paint(noseve, this);
-    }
-
     private void paintCasillas() {
         for(int i = 0; i < ROWS; i++){
             for(int j = 0; j < COLUMNS; j++)
                 casillas[i][j].paint(noseve);
         }
+    }
+    
+    private void paintFichas() {
+        for(int i = 0; i < NFICHAS; i++)
+            for(int j = 0; j < NFICHAS; j++)
+                fichas[i][j].paint(noseve, this);
     }
 
     private void paintNumeroAleatorio() {
@@ -263,5 +162,104 @@ public class Ruleta extends Applet implements Runnable {
                 noseve.drawString("FALTA", 490, 200);
             else
                 noseve.drawString("PASA", 490, 200);
+    }
+        
+    private void paintUltimas10jugadas() {
+        noseve.setColor(Color.BLACK);
+        for(int i = 0; i < ultimas10jugadas.length; i++){
+            noseve.drawRect(300 + 50*i, 300, 50, 50);
+            if(ultimas10jugadas[i] == 0)
+                noseve.setColor(Color.WHITE);
+            else if(lRojos.contains(ultimas10jugadas[i]))
+                noseve.setColor(Color.RED);
+            else
+                noseve.setColor(Color.BLACK);            
+            noseve.drawString(Integer.toString(ultimas10jugadas[i]), 310 + 50*i, 335);
+            noseve.setColor(Color.BLACK);
+        }
+    }
+    
+    public void update(Graphics g){ //override, lo sobreescribimos eliminando la linea de borrar
+        paint(g);  
+    }
+    
+    public void run(){
+        while(true){  
+        }
+    }
+    
+    public boolean action(Event ev, Object obj){
+        if(ev.target instanceof Button){
+            numeroAleatorio = (int)(Math.random() * 37);
+            guardarUltimas10jugadas();
+            comprobarApuesta();  
+            repaint();
+            return true;
+        }  
+        return false;
+    }
+    
+    private void guardarUltimas10jugadas() {
+        for(int i = ultimas10jugadas.length - 1 ; i > 0 ; i--)
+            ultimas10jugadas[i] = ultimas10jugadas[i -1];
+        ultimas10jugadas[0] = numeroAleatorio;
+    }
+    
+    private void comprobarApuesta() {
+        acumuladorUltimaJugada = 0;
+        for(int i = 0; i < NFICHAS; i++)
+            for(int j = 0; j < NFICHAS; j++){
+                comprobarApuestaANumero(i, j);
+                restarValorFichaApostada(i, j);
+            }
+        saldo += acumuladorUltimaJugada;
+    }
+
+    private void comprobarApuestaANumero(int i, int j) {
+        for(Integer elemento : fichas[i][j].getApuestas())
+            if(numeroAleatorio == elemento){
+                acumuladorUltimaJugada += fichas[i][j].getValor() * NUMCASILLAS/fichas[i][j].getApuestas().size();
+                System.out.println("valor ficha: " + fichas[i][j].getValor());
+                System.out.println("tamaño lista apuesta : " + fichas[i][j].getApuestas().size());
+                System.out.println("acumulado: " + fichas[i][j].getValor() * NUMCASILLAS/fichas[i][j].getApuestas().size());
+            }
+    }
+    
+    private void restarValorFichaApostada(int i, int j) {
+        if(!fichas[i][j].getApuestas().isEmpty()){
+            acumuladorUltimaJugada -= fichas[i][j].getValor();
+            System.out.println("Ficha jugada: -" + fichas[i][j].getValor());
+        }
+    }
+    
+    @Override
+    public boolean mouseDown(Event ev, int x, int y){
+       for(int i = 0; i < NFICHAS; i++)
+            for(int j = 0; j < NFICHAS; j++)
+                if(fichas[i][j].contains(x, y))
+                    fichaActual = fichas[i][j];
+        fichaActual.clearApuestas();
+        return true;
+    } 
+    
+    @Override
+    public boolean mouseDrag(Event ev, int x, int y){
+        if(fichaActual == null)
+            return false;
+        
+        fichaActual.move(x, y);
+        repaint();
+        return true;
+    }
+    
+    @Override
+    public boolean mouseUp(Event ev, int x, int y){
+        if(fichaActual == null)
+            return false;
+                 
+        fichaActual.setApuestas(casillas);         
+        fichaActual = null;
+            
+        return true;
     }
 }
