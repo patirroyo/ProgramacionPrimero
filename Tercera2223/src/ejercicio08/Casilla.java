eafc0332378e15a3d3993048d2697a0fc2dfa305
Alberto Saz <albertosaz@gmail.com>
2023-05-10 19:40:20 +0200

    Renombrado

--- Tercera2223/src/ejercicio08/Casilla.java
+++ Tercera2223/src/ejercicio08/Casilla.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio08;
+package Ejercicio08;
 
 import java.applet.Applet;
 import java.awt.Color;
