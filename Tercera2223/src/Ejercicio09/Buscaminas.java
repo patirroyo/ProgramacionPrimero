9e1ae46fd21d6106aec4612593bc0ef35df779d6
Alberto Saz <albertosaz@gmail.com>
2023-05-17 13:38:21 +0200

    buscaminas casi acabado

--- Tercera2223/src/Ejercicio09/Buscaminas.java
+++ Tercera2223/src/Ejercicio09/Buscaminas.java
@@ -6,33 +6,42 @@
 package Ejercicio09;
 
 import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Event;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.HeadlessException;
 import java.awt.Image;
+import java.awt.Label;
+
+import java.awt.Panel;
+import java.awt.TextField;
+
 
 /**
  *
  * @author alberto
  */
-public class Buscaminas extends Applet implements Runnable {
-    public static final int FILAS = 10;
-    public static final int COLUMNAS = 10;
-    public final static int SIZEX = 500;
-    public final static int SIZEY = 500;
+public class Buscaminas extends Applet{
+    public static final int DIM = 20;
+    public static final int TOP = 100;
+    public static final int SIZEX = Casilla.LADO*DIM;
+    public static final int SIZEY = SIZEX + TOP;
     private Image imagen; 
     private Graphics noseve;
-    private Thread animacion;
-    private int delay = 200;
-    private Image reverso;
-    private Image[] imagenes;
+    private Image mina;
+    private Image bandera;
     private Casilla[][] casillas;
-    private Casilla activa1;
-    private Casilla activa2;
-    private int contador;
-    private int descubiertas;
-    private int fallos;
+    private int[] aleatorios;
+    private int casillasVistas;
+    private int minasPorEncontrar;
+    private boolean win;
+    private TextField minas;
+    
+    
+
     
     public void init(){
         this.setSize(SIZEX, SIZEY);
@@ -40,42 +49,104 @@ public class Buscaminas extends Applet implements Runnable {
         imagen = this.createImage(SIZEX, SIZEY);
         noseve = imagen.getGraphics(); 
         
+        mina = getImage(getCodeBase(), "Ejercicio09/Imagenes/mina.png");
+        bandera = getImage(getCodeBase(), "Ejercicio09/Imagenes/bandera.png");
         
-        reverso = getImage(getCodeBase(), "Ejercicio09/Imagenes/casilla.png");
         
-        
-        casillas = new Casilla[FILAS][COLUMNAS];
-        for(int i = 0; i < FILAS; i++)
-            for(int j = 0; j < COLUMNAS; j++)   
-                casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO, reverso);
-        
+        setupPanels();
         
 
+        newGame();
+        
+      
+        
     }
 
-    
-    public void start(){
-        animacion = new Thread(this);
-        animacion.start();
+    private void newGame() {
+        win = false;
+        crearCasillas();
+        repartirMinas();
+        contarMinasAlrededor();
+        minas.setText(""+DIM);
     }
+
+    private void setupPanels() throws HeadlessException {
+        Panel panel = new Panel();
+        Button boton1 = new Button("Reiniciar !");
+        Label etiqueta = new Label("Minas Pendientes: ", Label.RIGHT);
+        minas = new TextField(""+DIM, 10);
+        minas.setEditable(false);
+        panel.add(etiqueta);
+        panel.add(minas);
+        panel.add(boton1);
+       
+        this.setLayout(new BorderLayout());
+        this.add("North", panel);
+      
+    }
+
+    private void crearCasillas() {
+        casillasVistas = 0;
+        casillas = new Casilla[DIM][DIM];
+        for(int i = 0; i < DIM; i++)
+            for(int j = 0; j < DIM; j++)
+                casillas[i][j] = new Casilla(j*Casilla.LADO, i*Casilla.LADO+TOP);
+    }
+
+    private void repartirMinas() {
+        minasPorEncontrar = 0;
+        aleatorios = obtenerAleatorios();
+        for(int i = 0; i < aleatorios.length; i++){
+            casillas[aleatorios[i]/DIM][aleatorios[i]%DIM].setMina(mina);
+            minasPorEncontrar++;
+        }
+    }    
+    public int[] obtenerAleatorios(){
+        int resultados[] = new int[DIM];
+        int vector[] = new int[DIM*DIM];
+        for(int i=0; i<DIM*DIM; i++)
+            vector[i] = i;
+        
+        for(int i=0; i<DIM; i++){
+            int aleatorio = (int)(Math.random()*((DIM*DIM)-i));
+            resultados[i]=vector[aleatorio];
+            vector[aleatorio]=vector[(DIM*DIM)-i-1];
+        }
+        return resultados;
+            
+    }
+    private void contarMinasAlrededor() {
+        for(int i = 0; i < DIM; i++)
+            for(int j = 0; j < DIM; j++){
+                int alrededor = 0;
+                for(int a = i-1; a <= i+1; a++){
+                    for(int b = j-1; b <= j+1; b++){
+                        if(a>=0 && b>=0 && a<DIM && b<DIM && casillas[a][b].isMina())
+                            alrededor++;
+                    }
+                }
+                casillas[i][j].setAlrededor(alrededor);
+            }
+    }
+   
     
     public void paint(Graphics g){
-        //noseve.fillRect(0, 0, SIZEX, SIZEY);
-        noseve.setColor(Color.BLACK);
+        noseve.setColor(Color.WHITE);
+        noseve.fillRect(0, 0, WIDTH, HEIGHT);
         
-        for(int i = 0; i < FILAS; i++)
-             for(int j = 0; j < COLUMNAS; j++){
+        for(int i = 0; i < DIM; i++)
+             for(int j = 0; j < DIM; j++){
                  casillas[i][j].paint(noseve, this);
                  
              }
         
-        
-        
-        
-        
-        
-      
-       g.drawImage(imagen, 0, 0, SIZEX, SIZEY, this);
+        if(win){
+            noseve.setColor(Color.WHITE);
+            noseve.fillRect(0, 0, WIDTH, HEIGHT);
+            noseve.setColor(Color.BLACK);
+            noseve.drawString("Has ganado", SIZEX/2, SIZEY/2);
+        }
+        g.drawImage(imagen, 0, 0, SIZEX, SIZEY, this);
     }
     
     public void update(Graphics g){ //override, lo sobreescribimos eliminando la linea de borrar
@@ -83,33 +154,46 @@ public class Buscaminas extends Applet implements Runnable {
         
     }
     
-    public void run(){
-        while(true){
-            contador++;
-            if(contador == 1000/delay){
-                repaint();
-            }
-            
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException ex){
-                System.out.println("Error en el hilo");
-            }
-        }
-    }
+   
     public boolean mouseDown(Event ev, int x, int y){
-        for(int i = 0; i < FILAS; i++)
-            for(int j = 0; j < COLUMNAS; j++)
+        for(int i = 0; i < DIM; i++)
+            for(int j = 0; j < DIM; j++)
                 if(casillas[i][j].contains(x,y)){
-                    casillas[i][j].destapar();
+                    if(!ev.metaDown())
+                        clickCasilla(i, j);
+                    else{
+                        casillas[i][j].setBandera(bandera);
+                        if(casillas[i][j].isConBandera())
+                            minasPorEncontrar--;
+                        else
+                            minasPorEncontrar++;
+                        minas.setText(""+minasPorEncontrar);
+                    }
                     repaint();
                 }
+        
         return false;
     } 
 
-    public boolean mouseUp(Event ev, int x, int y){
-        
-
-        return true;
+    public void clickCasilla(int i, int j){
+        if(casillas[i][j].isTapada()){
+            casillas[i][j].setDestapada();
+            casillasVistas++;
+            if(casillasVistas == DIM*DIM-DIM){
+                win = true;
+            } else if(casillas[i][j].getAlrededor()==0)
+                for(int i2 = i-1; i2 <= i+1;i2++)
+                    for(int j2 = j-1; j2 <= j+1; j2++)
+                        if(i2>=0 && j2>=0 && i2<DIM && j2<DIM)
+                            clickCasilla(i2, j2);
+            }
     } 
+    public boolean action(Event ev, Object obj){
+        if(ev.target instanceof Button){
+            newGame();
+        repaint();
+        
+        }
+        return false;
+    }
 }
--- Tercera2223/src/Ejercicio09/Casilla.java
+++ Tercera2223/src/Ejercicio09/Casilla.java
@@ -7,6 +7,7 @@ package Ejercicio09;
 
 import java.applet.Applet;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
@@ -16,28 +17,52 @@ import java.awt.Rectangle;
  * @author alberto
  */
 public class Casilla extends Rectangle {
-    public static final int LADO = 50;
+    public static final int LADO = 30;
     private Image mina;
-    private Image reverso;
     private Boolean tapada;
     private int alrededor;
+    private Boolean conBandera;
+    private Image bandera;
     
-    public Casilla(int posX, int posY, Image image){
+    public Casilla(int posX, int posY){
         super(posX, posY, LADO, LADO);
         tapada = true;
-        reverso = image;
+        conBandera = false;
         alrededor = 0;
     }
     
     public void paint(Graphics gg, Applet ap){
-        gg.setColor(Color.BLACK);
-        gg.drawRect(x, y, width, height);
-        if(tapada)
-            gg.drawImage(reverso, x, y, width, height, ap);
-        else if(mina != null)
+        if(mina != null&&!tapada){
+            gg.setColor(Color.RED);
+            gg.draw3DRect(x, y, width, height, tapada);
             gg.drawImage(mina, x, y, width, height, ap);
-        else
-            gg.drawString("" + alrededor, x+15, y+40);
+        }
+        else{
+            gg.setColor(Color.LIGHT_GRAY);
+            gg.fill3DRect(x, y, width, height, tapada);
+            switch(alrededor){
+                case 0:
+                    gg.setColor(Color.BLACK);
+                    break;
+                case 1:
+                    gg.setColor(Color.CYAN);
+                    break;
+                case 2:
+                    gg.setColor(Color.GREEN);
+                    break;
+                case 3:
+                    gg.setColor(Color.RED);
+                    break;
+                case 4:
+                    gg.setColor(Color.BLUE);
+            }
+            
+            gg.setFont(new Font("Arial", Font.PLAIN, 20));
+            gg.drawString((alrededor==0||tapada)?"":"" + alrededor, x+10, y+22);
+            if(conBandera)
+                gg.drawImage(bandera, x, y, width, height, ap);
+        }
+            
         
     }
 
@@ -45,16 +70,38 @@ public class Casilla extends Rectangle {
         return tapada;
     }
 
-    public void destapar() {
+    public void setDestapada() {
         this.tapada = false;
     }
+    public void setBandera(Image imagen) {
+        bandera = imagen;
+        if(conBandera)
+            conBandera = false;
+        else
+            conBandera = true;
+    }
 
-    public Image getMina() {
-        return mina;
+    public boolean isMina() {
+        if(mina == null)
+            return false;
+        else
+            return true;
     }
 
     public void setMina(Image imagen) {
         mina = imagen;
+    }
+
+    public int getAlrededor() {
+        return alrededor;
+    }
+
+    public void setAlrededor(int alrededor) {
+        this.alrededor = alrededor;
+    }
+
+    public Boolean isConBandera() {
+        return conBandera;
     }
     
 }
